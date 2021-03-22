390fabbb00bb3fc736ac29bea34f09aacb228a8c
Catherine Gilbert <catherine.gilbert15@gmail.com>
2021-03-22 16:34:30 -0400

    [BRV-2] Improved script to control arm movement

---
+++ Assets/Robot_arm/Scripts/EditorMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorMovement : MonoBehaviour
+{
+    [Range(Movement.J2BaseMinRotation, Movement.J2BaseMaxRotation)]
+    public float j2BaseRotation;
+
+    [Range(Movement.J2ArmMinRotation, Movement.J2ArmMaxRotation)]
+    public float j2ArmRotation;
+
+    [Range(Movement.J3HousingMinRotation, Movement.J3HousingMaxRotation)]
+    public float j3HousingRotation;
+
+    [Range(Movement.J4ArmMinRotation, Movement.J4ArmMaxRotation)]
+    public float j4ArmRotation;
+
+    [Range(Movement.J5WristMinRotation, Movement.J5WristMaxRotation)]
+    public float j5WristRotation;
+
+    [Range(Movement.J6MinRotation, Movement.J6MaxRotation)]
+    public float j6Rotation;
+
+    private Movement movement;
+
+    private void Start()
+    {
+        movement = GetComponent<Movement>();
+    }
+    
+    private void Update()
+    {
+        movement.J2BaseRotation = j2BaseRotation;
+        movement.J2ArmRotation = j2ArmRotation;
+        movement.J3HousingRotation = j3HousingRotation;
+        movement.J4ArmRotation = j4ArmRotation;
+        movement.J5WristRotation = j5WristRotation;
+        movement.J6Rotation = j6Rotation;
+    }
+}
--- Assets/Robot_arm/Scripts/Movement.cs
+++ Assets/Robot_arm/Scripts/Movement.cs
@@ -3,37 +3,91 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class Movement : MonoBehaviour
 {
-    [Range(-360, 360)] public float j2BaseRotation;
-    [Range(-100, 130)] public float j2ArmRotation;
-    [Range(-100, 65)] public float j3HousingRotation;
-    [Range(-180, 180)] public float j4ArmRotation;
-    [Range(-90, 90)] public float j5WristRotation;
-    [Range(-360, 360)] public float j6Rotation;
+    public const float J2BaseMinRotation = -360f;
+    public const float J2BaseMaxRotation = 360f;
+    public const float J2ArmMinRotation = -100f;
+    public const float J2ArmMaxRotation = 130f;
+    public const float J3HousingMinRotation = -100f;
+    public const float J3HousingMaxRotation = 65f;
+    public const float J4ArmMinRotation = -180f;
+    public const float J4ArmMaxRotation = 180f;
+    public const float J5WristMinRotation = -90f;
+    public const float J5WristMaxRotation = 90f;
+    public const float J6MinRotation = -360f;
+    public const float J6MaxRotation = 360f;
 
-    private GameObject J2Base;
-    private GameObject J2Arm;
-    private GameObject J3Housing;
-    private GameObject J4Arm;
-    private GameObject J5Wrist;
-    private GameObject J6;
+    private float j2BaseRotation;
+
+    public float J2BaseRotation
+    {
+        get => j2BaseRotation;
+        set => j2BaseRotation = Mathf.Clamp(value, J2BaseMinRotation, J2BaseMaxRotation);
+    }
+
+    private float j2ArmRotation;
+
+    public float J2ArmRotation
+    {
+        get => j2ArmRotation;
+        set => j2ArmRotation = Mathf.Clamp(value, J2ArmMinRotation, J2ArmMaxRotation);
+    }
+
+    private float j3HousingRotation;
+
+    public float J3HousingRotation
+    {
+        get => j3HousingRotation;
+        set => j3HousingRotation = Mathf.Clamp(value, J3HousingMinRotation, J3HousingMaxRotation);
+    }
+
+    private float j4ArmRotation;
+
+    public float J4ArmRotation
+    {
+        get => j4ArmRotation;
+        set => j4ArmRotation = Mathf.Clamp(value, J4ArmMinRotation, J4ArmMaxRotation);
+    }
+
+    private float j5WristRotation;
+
+    public float J5WristRotation
+    {
+        get => j5WristRotation;
+        set => j5WristRotation = Mathf.Clamp(value, J5WristMinRotation, J5WristMaxRotation);
+    }
+
+    private float j6Rotation;
+
+    public float J6Rotation
+    {
+        get => j6Rotation;
+        set => j6Rotation = Mathf.Clamp(value, J6MinRotation, J6MaxRotation);
+    }
+
+    private GameObject j2Base;
+    private GameObject j2Arm;
+    private GameObject j3Housing;
+    private GameObject j4Arm;
+    private GameObject j5Wrist;
+    private GameObject j6;
 
     private void Start()
     {
-        J2Base = GameObject.Find("j2base-rotation");
-        J2Arm = GameObject.Find("j2arm-rotation");
-        J3Housing = GameObject.Find("j3housing-rotation");
-        J4Arm = GameObject.Find("j4arm");
-        J5Wrist = GameObject.Find("j5wrist-rotation");
-        J6 = GameObject.Find("j6");
+        j2Base = GameObject.Find("j2base-rotation");
+        j2Arm = GameObject.Find("j2arm-rotation");
+        j3Housing = GameObject.Find("j3housing-rotation");
+        j4Arm = GameObject.Find("j4arm");
+        j5Wrist = GameObject.Find("j5wrist-rotation");
+        j6 = GameObject.Find("j6");
     }
 
     private void Update()
     {
-        J2Base.transform.rotation = Quaternion.Euler(0, j2BaseRotation, 0);
-        J2Arm.transform.rotation = J2Base.transform.rotation * Quaternion.Euler(0, 0, j2ArmRotation);
-        J3Housing.transform.rotation = J2Arm.transform.rotation * Quaternion.Euler(0, 0, j3HousingRotation);
-        J4Arm.transform.rotation = J3Housing.transform.rotation * Quaternion.Euler(j4ArmRotation, 0, 0);
-        J5Wrist.transform.rotation = J4Arm.transform.rotation * Quaternion.Euler(0, 0, j5WristRotation);
-        J6.transform.rotation = J5Wrist.transform.rotation * Quaternion.Euler(j6Rotation, 0, 90);
+        j2Base.transform.rotation = Quaternion.Euler(0, j2BaseRotation, 0);
+        j2Arm.transform.rotation = j2Base.transform.rotation * Quaternion.Euler(0, 0, j2ArmRotation);
+        j3Housing.transform.rotation = j2Arm.transform.rotation * Quaternion.Euler(0, 0, j3HousingRotation);
+        j4Arm.transform.rotation = j3Housing.transform.rotation * Quaternion.Euler(j4ArmRotation, 0, 0);
+        j5Wrist.transform.rotation = j4Arm.transform.rotation * Quaternion.Euler(0, 0, j5WristRotation);
+        j6.transform.rotation = j5Wrist.transform.rotation * Quaternion.Euler(j6Rotation, 0, 90);
     }
 }
