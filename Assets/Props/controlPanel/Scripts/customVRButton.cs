ca8c0969a5e7e154796d97074d695e4cf820881d
Catherine Gilbert <catherine.gilbert15@gmail.com>
2021-03-24 13:44:07 -0400

    [BRV-6] Improved button feedback

--- Assets/Props/controlPanel/Scripts/customVRButton.cs
+++ Assets/Props/controlPanel/Scripts/customVRButton.cs
@@ -13,6 +13,11 @@ public class customVRButton : MonoBehaviour
     public bool pressed;
     public ButtonEvent downEvent;
 
+
+    public Material defaultMaterial;
+    public Material pressedMaterial;
+    private MeshRenderer meshRenderer;
+    
     private Vector3 startPos;
 
     private SpringJoint springJoint;
@@ -21,12 +26,15 @@ public class customVRButton : MonoBehaviour
     private void Start()
     {
         startPos = transform.localPosition;
+        meshRenderer = GetComponent<MeshRenderer>();
         springJoint = GetComponent<SpringJoint>();
         localConstraints = GetComponent<PositionLocalConstraints>();
     }
 
     private void Update()
     {
+        bool originalPressed = pressed;
+        
         float distance = Mathf.Abs(transform.localPosition.y - startPos.y);
         if (distance >= pressLength)
         {
@@ -41,6 +49,11 @@ public class customVRButton : MonoBehaviour
         else
         {
             pressed = false;
+        }
+
+        if (originalPressed != pressed)
+        {
+            meshRenderer.material = pressed ? pressedMaterial : defaultMaterial;
         }
 
         if (transform.localPosition.y > startPos.y)
