7fece690113028207079545e5be9edf469a8b9a6
P <pat.morin17@hotmail.com>
2021-04-19 15:24:36 -0400

    [BRV-5] Added overShoulder inventory.

---
+++ Assets/OverShoulderInventory.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class OverShoulderInventory : MonoBehaviour
+{
+    private Hand[] hands;
+    private List<GameObject> objectsInCollider;
+    private GameObject storedItem;
+
+    void Start()
+    {
+        hands = FindObjectsOfType<Hand>();
+        objectsInCollider = new List<GameObject>();
+    }
+
+    void Update()
+    {
+        foreach (var hand in hands)
+        {
+            foreach (var objectInCollider in objectsInCollider)
+            {
+                if (hand.gameObject.GetInstanceID() == objectInCollider.GetInstanceID())
+                {
+                    if (hand.IsGrabEnding(hand.currentAttachedObject) && !storedItem &&
+                        !hand.currentAttachedObject.CompareTag("Target"))
+                    {
+                        storedItem = hand.currentAttachedObject;
+                        hand.DetachObject(hand.currentAttachedObject);
+                        storedItem.SetActive(false);
+                    }
+
+                    if (hand.GetGrabStarting() != GrabTypes.None && storedItem)
+                    {
+                        storedItem.SetActive(true);
+                        hand.AttachObject(storedItem, GrabTypes.Grip);
+                        storedItem = null;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        objectsInCollider.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        objectsInCollider.Remove(other.gameObject);
+    }
+}
