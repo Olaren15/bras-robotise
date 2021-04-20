bc829b98b99e87b4a2c16f82146f773dff1c2ad9
Catherine Gilbert <catherine.gilbert15@gmail.com>
2021-04-20 15:02:41 -0400

    [BRV-8] New sequences are now saved on disk

--- Assets/Robot_arm/Scripts/KeyPoint.cs
+++ Assets/Robot_arm/Scripts/KeyPoint.cs
@@ -1,17 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Robot_arm.Scripts
 {
+    [Serializable]
     public class KeyPoint
     {
-        public Vector3 targetPosition { get;}
-        public Vector3 targetRotationPosition { get;}
+        // target position
+        private float t1x, t1y, t1z;
+
+        // target rotation position
+        private float t2x, t2y, t2z;
+
+        public Vector3 TargetPosition => new Vector3(t1x, t1y, t1z);
+        public Vector3 TargetRotationPosition => new Vector3(t2x, t2y, t2z);
 
         public KeyPoint(Vector3 targetPosition, Vector3 targetRotationPosition)
         {
-            this.targetPosition = targetPosition;
-            this.targetRotationPosition = targetRotationPosition;
-        }
+            t1x = targetPosition.x;
+            t1y = targetPosition.y;
+            t1z = targetPosition.z;
 
+            t2x = targetRotationPosition.x;
+            t2y = targetRotationPosition.y;
+            t2z = targetRotationPosition.z;
+        }
     }
 }
--- Assets/Robot_arm/Scripts/SequenceRecorder.cs
+++ Assets/Robot_arm/Scripts/SequenceRecorder.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using Robot_arm.Scripts;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,12 +16,40 @@ public class SequenceRecorder : MonoBehaviour
     public void StartRecording()
     {
         SetRecording(true);
+
+        foreach (GameObject gameObject in sequenceGameObjects)
+        {
+            Destroy(gameObject);
+        }
+
+        sequenceGameObjects.Clear();
     }
 
     public List<KeyPoint> StopRecording()
     {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/sequence.dat", FileMode.Create);
+        binaryFormatter.Serialize(file, sequence);
+        file.Close();
+
+        if (File.Exists(Application.persistentDataPath + "/sequence.dat"))
+        {
+            BinaryFormatter binaryFormatter1 = new BinaryFormatter();
+            FileStream file1 = File.Open(Application.persistentDataPath + "/sequence.dat", FileMode.Open);
+            var savedData = binaryFormatter1.Deserialize(file1) as List<KeyPoint>;
+            file1.Close();
+
+            for (int i = 0; i < savedData.Count; i++)
+            {
+                print("Target position " + i);
+                print(savedData[i].TargetPosition);
+                print("Target rotation position " + i);
+                print(savedData[i].TargetRotationPosition);
+            }
+        }
+
         SetRecording(false);
-        List<KeyPoint> newSequence = sequence.ToList();
+        var newSequence = sequence.ToList();
         sequence.Clear();
         return newSequence;
     }
