c0c8ef5ffb06bdd0866fd0da221b27fa1963d28f
P <pat.morin17@hotmail.com>
2021-04-22 14:28:15 -0400

    Fixed IK bug related to the Movement.cs script

--- Assets/Props/controlPanel/Scripts/ResetButton.cs
+++ Assets/Props/controlPanel/Scripts/ResetButton.cs
@@ -9,14 +9,26 @@ public class ResetButton : MonoBehaviour
     public GameObject targetRotation;
     private Vector3 initialTargetPosition;
     private Vector3 initialTargetRotationPosition;
-    private Movement movement;
 
+    private GameObject j2Base;
+    private GameObject j2Arm;
+    private GameObject j3Housing;
+    private GameObject j4Arm;
+    private GameObject j5Wrist;
+    private GameObject j6;
     
     private void Start()
     {
         initialTargetPosition = target.transform.position;
         initialTargetRotationPosition = targetRotation.transform.position;
-        movement = FindObjectOfType<Movement>();
+        
+        //for an absolute reset
+        j2Base = GameObject.Find("j2base-rotation");
+        j2Arm = GameObject.Find("j2arm-rotation");
+        j3Housing = GameObject.Find("j3housing-rotation");
+        j4Arm = GameObject.Find("j4arm-rotation");
+        j5Wrist = GameObject.Find("j5wrist-rotation");
+        j6 = GameObject.Find("j6");
     }
 
     public void ResetRobotPosition()
@@ -25,12 +37,12 @@ public class ResetButton : MonoBehaviour
         targetRotation.transform.position = initialTargetRotationPosition;
         targetRotation.transform.rotation = Quaternion.identity;
         
-        movement.J6Rotation = 0;
-        movement.J5WristRotation = 0;
-        movement.J4ArmRotation = 0;
-        movement.J3HousingRotation = 0;
-        movement.J2ArmRotation = 0;
-        movement.J2BaseRotation = 0;
+        j2Base.transform.rotation = Quaternion.identity;
+        j2Arm.transform.rotation = Quaternion.identity;
+        j3Housing.transform.rotation = Quaternion.identity;
+        j4Arm.transform.rotation = Quaternion.identity;
+        j5Wrist.transform.rotation = Quaternion.identity;
+        j6.transform.rotation = Quaternion.Euler(0, 0, 90);
 
 
     }
