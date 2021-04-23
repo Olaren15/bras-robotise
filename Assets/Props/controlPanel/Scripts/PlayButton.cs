d6fc8bec56a10d94e569e2db8498c11b095d8a6b
Catherine Gilbert <catherine.gilbert15@gmail.com>
2021-04-23 14:17:16 -0400

    [BRV-10] reset robot position when starting playback

--- Assets/Props/controlPanel/Scripts/PlayButton.cs
+++ Assets/Props/controlPanel/Scripts/PlayButton.cs
@@ -5,18 +5,21 @@ public class PlayButton : MonoBehaviour
     private SequenceList sequenceList;
     private SequencePlayer sequencePlayer;
     private SequenceRecorder sequenceRecorder;
+    private ResetButton reset;
 
     private void Start()
     {
         sequenceList = FindObjectOfType<SequenceList>();
         sequencePlayer = FindObjectOfType<SequencePlayer>();
         sequenceRecorder = FindObjectOfType<SequenceRecorder>();
+        reset = FindObjectOfType<ResetButton>();
     }
 
     public void Play()
     {
         if (sequenceList.currentSequence != null && !sequenceRecorder.recording && !sequencePlayer.playing)
         {
+            reset.ResetRobotPosition();
             sequencePlayer.Play(sequenceList.currentSequence);
         }
     }
