3617a1839bafb1f2f3a785142b956ca9106464f2
Catherine Gilbert <catherine.gilbert15@gmail.com>
2021-04-26 13:24:36 -0400

    Fix hand collision bug when removing control panel from inventory

---
+++ Assets/Props/controlPanel/Scripts/FixHandCollision.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+
+public class FixHandCollision : MonoBehaviour
+{
+    public void DisableCollider()
+    {
+        Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("button"), true);
+        StartCoroutine(WaitForSec(1));
+    }
+
+    private IEnumerator WaitForSec(float sec)
+    {
+        yield return new WaitForSeconds(sec);
+        Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("button"), false);
+    }
+
+    private void OnEnable()
+    {
+        Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("button"), false);
+    }
+}
