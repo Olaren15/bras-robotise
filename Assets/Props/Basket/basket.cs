97f8751a6c9cf4b12520f9be4413414f315f22a9
P <pat.morin17@hotmail.com>
2021-04-30 14:37:31 -0400

    [BRV-19] Added audio source when player score a goal

--- Assets/Props/Basket/basket.cs
+++ Assets/Props/Basket/basket.cs
@@ -1,11 +1,19 @@
+using System;
 using UnityEngine;
 
 public class basket : MonoBehaviour
 {
     public GameObject confetti;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Instantiate(confetti, GameObject.Find("ConfettiSpawner").transform.position, Quaternion.Euler(90, 0, 0));
+        audioSource.Play();
     }
 }
